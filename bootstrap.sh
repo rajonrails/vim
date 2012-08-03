55d6c2502e526eeddfe7ce1f6df818a1a64c5655
Rajesh Parvathaneni <rajeshparvathaneni@gmail.com>
2012-08-03 12:01:48 -0700

    dot files

---
+++ bootstrap.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+<<<<<<< HEAD
+=======
+
+endpath="$HOME/.spf13-vim-3"
+>>>>>>> 3.1
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+echo "thanks for installing spf13-vim\n"
+
+# Backup existing .vim stuff
+echo "backing up current vim config\n"
+today=`date +%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $i.$today; done
+
+
+echo "cloning spf13-vim\n"
+git clone --recursive -b 3.0 http://github.com/rajonrails/spf13-vim.git $endpath
+mkdir -p $endpath/.vim/bundle
+ln -s $endpath/.vimrc $HOME/.vimrc
+ln -s $endpath/.vim $HOME/.vim
+
+echo "Installing Vundle"
+git clone http://github.com/rajonrails/vundle.git $HOME/.vim/bundle/vundle
+
+echo "installing plugins using Vundle"
+vim +BundleInstall! +BundleClean +q
